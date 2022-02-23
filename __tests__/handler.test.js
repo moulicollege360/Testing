ace219209f8b875bd01005173d16dafa9249234a
Chandramouli <98220086+moulicollege360@users.noreply.github.com>
2022-02-23 20:38:56 +0530

    Update handler.test.js

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,5 +1,5 @@
 const handler = require('../handler');
  
 test('correct greeting is generated', () => {
-  expect(handler.printHelloWorld()).toBe("Hello World");
+  expect(handler.printHelloWorld()).toBe("Hello World to mouli");
 });
